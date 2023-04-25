7e56d3db5616570247d61e68492bd19bd2c1925a
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-25 15:36:21 -0300

    Ajustes finais no menu

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,13 @@ const icone = document.querySelector(".icone");
 botao.addEventListener("click", function(event){
     event.preventDefault();
     linksMenu.classList.toggle("aberto");
+
+    /* Se a classe "aberto" estiver aplicada 
+    ao linksMenu, mude o texto/ícone para "Fechar &times;".
+    Caso contrário, mostre o texto/ícone "Menu &equiv;" */
+    if(linksMenu.classList.contains("aberto")){
+        icone.innerHTML = "Fechar &times;";
+    } else {
+        icone.innerHTML = "Menu &equiv;";
+    }
 });
