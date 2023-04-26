8c72adc5651e16c447c75ab6efbdf6b5b6797469
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-04-26 15:28:52 -0300

    Recursos jQuery
    jQuery Mask

--- js/contato.js
+++ js/contato.js
@@ -1,12 +1,19 @@
 /* Selecionando elementos a serem manipulados */
 const formulario = document.querySelector("form");
 const campoCep = formulario.querySelector("#cep");
+const campoTelefone = formulario.querySelector("#telefone");
 const campoEndereco = formulario.querySelector("#endereco");
 const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
-const status = formulario.querySelector("#status");
+const mensagem = formulario.querySelector("#status");
 const botaoLocalizar = formulario.querySelector("#localizar-cep");
+
+/* Ativação das máscaras com jQuery Mask */
+$(campoCep).mask("00000-000");
+$(campoTelefone).mask("(00) 0000-0000");
+
+
 
 /* Monitorando o evento de acionamento do botão localizar cep */
 botaoLocalizar.addEventListener("click", function(event){
@@ -40,11 +47,15 @@ botaoLocalizar.addEventListener("click", function(event){
         // Se existir o indicador "erro" no objeto dados
         if( "erro" in dados ){
             // Apresentamos a mensagem abaixo
-            console.log("CEP não encontrado!");
+            mensagem.innerHTML = "CEP não encontrado!";
+            mensagem.style.color = "red";
+            campoCep.focus();
         } else {
             // Senão, o cep existe então mostramos:
-            console.log("CEP encontrado!");
+            mensagem.innerHTML = "CEP encontrado!";
+            mensagem.style.color = "blue";
 
+            // Mostramos os dados nos campos
             campoEndereco.value = dados.logradouro;
             campoBairro.value = dados.bairro;
             campoCidade.value = dados.localidade;
